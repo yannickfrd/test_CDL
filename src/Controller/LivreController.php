a4b509a0c3904e32e3f8cc356e39199f01c1a8c2
yannickfrd <farade.y@hotmail.fr>
2021-02-08 17:34:38 +0100

    - filter livre index

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -2,21 +2,104 @@
 
 namespace App\Controller;
 
+use App\Entity\FilterLivre;
+use App\Entity\Livre;
+use App\Form\FilterLivreSearchType;
+use App\Form\LivreType;
 use App\Repository\LivreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/livre")
+ */
 class LivreController extends AbstractController
 {
     /**
-     * @Route("/livre", name="livre")
+     * @Route("/", name="livre_index", methods={"GET"})
      */
-    public function index(LivreRepository $livreRepository): Response
+    public function index(LivreRepository $livreRepository, Request $request): Response
     {
+        $search = new FilterLivre();
+        $form = $this->createForm(FilterLivreSearchType::class, $search)->handleRequest($request);
+
+        $query = $livreRepository->findAll();
+        if ( $form->isSubmitted() && $form->isValid() ) {
+            $query = $livreRepository->SearchQuery($search);
+        }
+
         return $this->render('livre/index.html.twig', [
-            'controller_name' => 'LivreController',
-            'livres' => $livreRepository->findAll()
+            'livres' => $query,
+            'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/new", name="livre_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $livre = new Livre();
+        $form = $this->createForm(LivreType::class, $livre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($livre);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('livre_index');
+        }
+
+        return $this->render('livre/new.html.twig', [
+            'livre' => $livre,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="livre_show", methods={"GET"})
+     */
+    public function show(Livre $livre): Response
+    {
+        return $this->render('livre/show.html.twig', [
+            'livre' => $livre,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="livre_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Livre $livre): Response
+    {
+        $form = $this->createForm(LivreType::class, $livre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('livre_index');
+        }
+
+        return $this->render('livre/edit.html.twig', [
+            'livre' => $livre,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="livre_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Livre $livre): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$livre->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($livre);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('livre_index');
     }
 }
